e19ee994da58cdc69ace817d0b8cfa317fdb6885
panda01real <marcospereyra07@gmail.com>
2026-05-07 19:28:01 -0300

    nav mesh basico
    me olvide de ir haciendo commits, el enemigo si detecta que se esta moviendo cambia de idle a running, hay que ver como detecta el bakeo en las escenas

---
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public Transform currentTarget;
+    private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
+    [SerializeField, Range(1f, 10f)] private float enemySpeed = 1f;
+
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponentInChildren<Animator>();
+        
+        _navMeshAgent.speed = enemySpeed;
+    }
+    
+    private void FixedUpdate()
+    {
+        if (currentTarget != null) ChaseState();
+        else PatrolState();
+    }
+
+    void ChaseState()
+    {
+        int speed = 3;
+        if (_navMeshAgent.velocity.magnitude != 0)
+        {
+            _animator.SetFloat("xAxis", Mathf.Lerp(_animator.GetFloat("xAxis"), -1, Time.deltaTime * speed));
+        }
+        _navMeshAgent.destination = currentTarget.position;
+    }
+
+    void PatrolState()
+    {
+        // que patrulle por la zona
+    }
+}
