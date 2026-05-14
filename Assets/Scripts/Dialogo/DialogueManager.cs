812db319f16c95679f06e988b6fd072c57816f82
panda01real <marcospereyra07@gmail.com>
2026-05-14 19:29:06 -0300

    cosinhas

--- Assets/Scripts/Dialogo/DialogueManager.cs
+++ Assets/Scripts/Dialogo/DialogueManager.cs
@@ -8,31 +8,33 @@ public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager Instance { get; private set; }
 
-    [Header("Componentes UI")]
-    public GameObject panelDialogo;
+    [Header("Componentes UI")] public GameObject panelDialogo;
     public TextMeshProUGUI textoNombre;
     public TextMeshProUGUI textoDialogo;
     public GameObject iconoContinuar;
 
-    [Header("Nota")]
-    public GameObject notaEnCamara;
+    [Header("Nota")] public GameObject notaEnCamara;
 
     private Queue<LineaDeDialogo> oraciones;
     private bool talking = false;
-    
+
     [SerializeField] Image interactButtonImage;
 
     void Awake()
     {
-        if (Instance == null) { Instance = this; }
-        else { Destroy(gameObject); }
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
 
         oraciones = new Queue<LineaDeDialogo>();
         panelDialogo.SetActive(false);
 
         if (notaEnCamara != null) notaEnCamara.SetActive(false);
-        
-        
     }
 
     private void Update()
@@ -57,6 +59,7 @@ public class DialogueManager : MonoBehaviour
             {
                 lineaProcesada.nombrePersonaje = dialogo.nombrePorDefecto;
             }
+
             oraciones.Enqueue(lineaProcesada);
         }
 
@@ -107,7 +110,7 @@ public class DialogueManager : MonoBehaviour
             if (notaEnCamara != null) notaEnCamara.SetActive(false);
         }
 
-            iconoContinuar.SetActive(oraciones.Count > 0);
+        iconoContinuar.SetActive(oraciones.Count > 0);
     }
 
     public void TerminarDialogo()
@@ -122,10 +125,10 @@ public class DialogueManager : MonoBehaviour
     {
         return talking;
     }
-    
+
     void InteractableSeen(bool isSeen)
     {
-        Debug.Log($"<color=red>INTERACTABLES IS SEEN {isSeen}</color>");
+        //Debug.Log($"<color=red>INTERACTABLES IS SEEN {isSeen}</color>");
         if (interactButtonImage != null)
         {
             interactButtonImage.enabled = isSeen;
@@ -147,5 +150,4 @@ public class DialogueManager : MonoBehaviour
         // Ya se desuscribe en OnDisable, pero lo dejamos por si acaso o lo removemos si OnDisable es suficiente.
         // En Unity, OnDisable se llama antes que OnDestroy.
     }
-
 }
