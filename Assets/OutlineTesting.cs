e763b69bc0718379435d7a9941461b3521fcc79b
Nahuefz <72934887+Nahuefz@users.noreply.github.com>
2026-05-15 22:30:54 -0300

    La concha de la re puta madre de los shaders
    no pense que iba a ser tanta paja

--- Assets/OutlineTesting.cs
+++ Assets/OutlineTesting.cs
@@ -1,24 +1,43 @@
 using System;
 using UnityEngine;
 
+using System;
+using UnityEngine;
+
 public class OutlineTesting : MonoBehaviour, IOutlined
 {
-   private Outline outline;
-   float _outlineWidth = 4;
+    private Outline outline;
+    [SerializeField] float _outlineWidth = 4f;
 
-   private void Awake()
-   {
-      outline = GetComponent<Outline>();
-      //_outlineWidth = outline.OutlineWidth;
-   }
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
 
-   public void DrawOutline(GameObject obj)
-   {
-      outline.OutlineWidth = _outlineWidth;
-   }
+        // Apagamos el outline al inicio para probar que el trigger funcione
+        if (outline != null) outline.OutlineWidth = 0f;
+    }
 
-   public void EraseOutline(GameObject obj)
-   {
-      outline.OutlineWidth = 0f;
-   }
+    public void DrawOutline(GameObject obj)
+    {
+        if (outline != null) outline.OutlineWidth = _outlineWidth;
+    }
+
+    public void EraseOutline(GameObject obj)
+    {
+        if (outline != null) outline.OutlineWidth = 0f;
+    }
+
+    // --- MÉTODOS DE PRUEBA CON EL MOUSE ---
+
+    // Se ejecuta cuando el puntero del mouse entra en el cubo
+    private void OnMouseEnter()
+    {
+        DrawOutline(this.gameObject);
+    }
+
+    // Se ejecuta cuando el puntero del mouse sale del cubo
+    private void OnMouseExit()
+    {
+        EraseOutline(this.gameObject);
+    }
 }
