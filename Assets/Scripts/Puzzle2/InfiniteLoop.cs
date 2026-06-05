7856310f369e138f610b13ff0f6323ee1b1befad
brunorquideas <bonato.b@gmail.com>
2026-06-05 18:51:45 -0300

    Puzzle 2
    Esta el mundo, esta el loop y hay un cubo para hablarle

---
+++ Assets/Scripts/Puzzle2/InfiniteLoop.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class InfiniteLoop : MonoBehaviour
+{
+    private float mapSize = 24f;
+    private float limit;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        limit = mapSize / 2f;
+        rb = GetComponent<Rigidbody>();
+
+        if (rb == null) Debug.LogError("Falta el componente Rigidbody");
+    }
+
+    void FixedUpdate()
+    {
+        if (rb == null) return;
+
+        Vector3 pos = rb.position;
+        bool huboTeletransporte = false;
+
+        if (pos.x > limit)
+        {
+            pos.x -= mapSize;
+            huboTeletransporte = true;
+        }
+        else if (pos.x < -limit)
+        {
+            pos.x += mapSize;
+            huboTeletransporte = true;
+        }
+
+        if (pos.z > limit)
+        {
+            pos.z -= mapSize;
+            huboTeletransporte = true;
+        }
+        else if (pos.z < -limit)
+        {
+            pos.z += mapSize;
+            huboTeletransporte = true;
+        }
+
+        if (huboTeletransporte)
+        {
+            rb.position = pos;
+        }
+    }
+}
