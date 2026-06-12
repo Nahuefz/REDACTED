ae197fe486eec4e05751d2f6dee4f7d14a02baf3
panda01real <marcospereyra07@gmail.com>
2026-06-12 16:45:29 -0300

    LAS COSAS, FUNCIONAN

---
+++ Assets/Scripts/Managers/UI/VisibilityMeter.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Enemy.ShyEnemy;
+
+public class VisibilityMeter : MonoBehaviour
+{
+    [SerializeField] private GameObject visibilityMeter;
+    private Image _visibilityMeterFill;
+    private Image _visibilityMeterIcon;
+
+    private void Awake()
+    {
+        // Buscamos los componentes necesarios
+        _visibilityMeterFill = visibilityMeter.transform.Find("Fill").GetComponent<Image>();
+        _visibilityMeterIcon = visibilityMeter.transform.Find("UI").GetComponent<Image>();
+        
+        // Inicializamos oculto
+        UpdateVisibilityUI(0f);
+    }
+
+    private void OnEnable()
+    {
+        // Nos suscribimos al evento estático de los enemigos tímidos
+        ShyEnemyBehaviour.OnVisibilityChanged += UpdateVisibilityUI;
+    }
+
+    private void OnDisable()
+    {
+        // Importante desuscribirse para evitar errores de memoria
+        ShyEnemyBehaviour.OnVisibilityChanged -= UpdateVisibilityUI;
+    }
+
+    private void UpdateVisibilityUI(float progress)
+    {
+        if (progress > 0)
+        {
+            // Si hay progreso, activamos y actualizamos
+            if (!visibilityMeter.activeSelf) visibilityMeter.SetActive(true);
+            
+            _visibilityMeterFill.enabled = true;
+            _visibilityMeterIcon.enabled = true;
+            _visibilityMeterFill.fillAmount = progress;
+        }
+        else
+        {
+            // Si no hay progreso, ocultamos el medidor
+            _visibilityMeterFill.fillAmount = 0;
+            _visibilityMeterFill.enabled = false;
+            _visibilityMeterIcon.enabled = false;
+            
+            if (visibilityMeter.activeSelf) visibilityMeter.SetActive(false);
+        }
+    }
+}
