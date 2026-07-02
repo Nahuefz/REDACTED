e032c051636745a8bfd4b3f1cb0646effad955d3
panda01real <marcospereyra07@gmail.com>
2026-07-02 03:23:13 -0300

    solucione el puzzle!
    todavia no hay implementacion de algo, pero ahora checkea si se llega a la solucion y tira un debuglog

--- Assets/Scripts/Puzzle/PantallaInteract.cs
+++ Assets/Scripts/Puzzle/PantallaInteract.cs
@@ -1,55 +1,65 @@
 using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PantallaInteract : MonoBehaviour, IOutlined, IInteractable
 {
     [SerializeField] private GameObject[] _pantallas;
-    [SerializeField] private Material[] colors;
-    [SerializeField] private PantallaColor colorReference;
-    [SerializeField] private Outline[] outline;
+    private Material[] colors;
+    private Outline[] outline;
 
+    private Bomb1Puzzle _puzzleLogic;
     private MeshRenderer[] _renderers;
-    private int[] _currentColorIndex;
-    [SerializeField]private int _focusedIndex = -1;
+    [SerializeField] private int[] _currentColorIndex;
+    [SerializeField] private int _focusedIndex = -1;
+
     private void Awake()
     {
-        colorReference = this.transform.parent.Find("Pantallas").GetComponent<PantallaColor>();
-        //_pantallas = colorReference._pantallas;
+        _puzzleLogic = GetComponentInParent<Bomb1Puzzle>();
+        if (_puzzleLogic != null)
+        {
+            colors = _puzzleLogic.ClueColors;
+        }
     }
-    
 
     private void Start()
     {
-        colors = colorReference._pantallaColor;
-
         outline = new Outline[_pantallas.Length];
         _renderers = new MeshRenderer[_pantallas.Length];
         _currentColorIndex = new int[_pantallas.Length];
 
         for (int i = 0; i < _pantallas.Length; i++)
         {
-            outline[i] = _pantallas[i].GetComponent<Outline>();
-            _renderers[i] = _pantallas[i].GetComponent<MeshRenderer>();
+            if (_pantallas[i] != null)
+            {
+                outline[i] = _pantallas[i].GetComponent<Outline>();
+                _renderers[i] = _pantallas[i].GetComponent<MeshRenderer>();
+            }
             _currentColorIndex[i] = 0;
         }
     }
+
     public void Interact(GameObject interactor)
     {
         Debug.Log($"Interact called, focused index = {_focusedIndex}");
         CyclicColorChange();
     }
+
     private void CyclicColorChange()
     {
         if (_focusedIndex < 0 || _focusedIndex >= _renderers.Length) return;
 
-        _currentColorIndex[_focusedIndex]++;
-        if (_currentColorIndex[_focusedIndex] >= colors.Length)
+        _currentColorIndex[_focusedIndex] = (_currentColorIndex[_focusedIndex] + 1) % colors.Length;
+        
+        if (_renderers[_focusedIndex] != null)
         {
-            _currentColorIndex[_focusedIndex] = 0;
+            _renderers[_focusedIndex].material = colors[_currentColorIndex[_focusedIndex]];
         }
 
-        _renderers[_focusedIndex].material = colors[_currentColorIndex[_focusedIndex]];
+        // Chequear si con este cambio se resolvió el puzzle
+        if (_puzzleLogic != null)
+        {
+            _puzzleLogic.CheckSolution(_currentColorIndex);
+        }
     }
     
     #region OutlineMethods
@@ -76,16 +86,21 @@ public class PantallaInteract : MonoBehaviour, IOutlined, IInteractable
     {
         if (outlineIndex < 0 || outlineIndex >= outline.Length) return;
 
-        outline[outlineIndex].OutlineWidth = 6.8f;
+        if (outline[outlineIndex] != null)
+        {
+            outline[outlineIndex].OutlineWidth = 6.8f;
+        }
         _focusedIndex = outlineIndex;
     }
-
 
     public void EraseOutline(int outlineIndex)
     {
         if (outlineIndex < 0 || outlineIndex >= outline.Length) return;
 
-        outline[outlineIndex].OutlineWidth = 0f;
+        if (outline[outlineIndex] != null)
+        {
+            outline[outlineIndex].OutlineWidth = 0f;
+        }
 
         if (_focusedIndex == outlineIndex)
         {
