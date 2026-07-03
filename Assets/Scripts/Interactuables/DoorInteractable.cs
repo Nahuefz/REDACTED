2a5a470e5e60fb4fc020544db6a121c89666ad2b
Nahuel <nahuelfontenez@gmail.com>
2026-07-03 05:15:45 -0300

    im just tired man
    hice la animeta, puse el pivote de la bisagra/visagra? de la puerta del baño para que cuando interactues

---
+++ Assets/Scripts/Interactuables/DoorInteractable.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class DoorInteractable : MonoBehaviour, IInteractable
+{
+    [Header("Componentes")]
+    [SerializeField] private Animator puertaAnimator; // Arrastrá acá el Animator de la puerta
+
+    private bool estaAbierta = false;
+
+    void Start()
+    {
+        if (puertaAnimator == null)
+        {
+            puertaAnimator = GetComponent<Animator>();
+        }
+    }
+
+    // Esta función la ejecuta el script de tus compañeros automáticamente
+    public void Interact(GameObject player)
+    {
+        if (puertaAnimator != null)
+        {
+            // Invertimos el estado actual
+            estaAbierta = !estaAbierta;
+
+            // Le pasamos el dato al Animator Controller
+            puertaAnimator.SetBool("isOpen", estaAbierta);
+
+            Debug.Log(estaAbierta ? "Baño: Abriendo puerta." : "Baño: Cerrando puerta.");
+        }
+    }
+}
